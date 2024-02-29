382e44a980adf04d55fadb27c28c7e44aa95ed6e
User <Willspri@gmail.com>
2024-02-29 16:48:05 +1300

    word lengths added

--- pythonMapping/pythonMapping.py
+++ pythonMapping/pythonMapping.py
@@ -3,3 +3,8 @@
 numbers = [1,2,3,4,5,6,7,8,9]
 squared = list(map(lambda x: x**2, numbers))
 print(squared)
+
+#string lengths
+words = ["foot", "house","grass", "smoke", "fish", "lamp", "elephant", "telephone"]
+lengths = list(map(len, words))
+print(lengths)
